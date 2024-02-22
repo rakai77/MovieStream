f973cdd7cd0b901e79b0ab187045c5700754f88c
rakaiwangsasatia <rakai77.rr@gmail.com>
2024-02-22 09:43:55 +0700

    initialize view in movie detail

---
+++ app/src/main/java/com/example/moviestream/presentation/detail/MovieDetailFragment.kt
@@ -0,0 +1,35 @@
+package com.example.moviestream.presentation.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.moviestream.databinding.FragmentMovieDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieDetailFragment : Fragment() {
+
+    private var _binding: FragmentMovieDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMovieDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
